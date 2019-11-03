38cef947dce6111eaef91a11fe90105226305b8f
klotkawa <56807286+klotkawa@users.noreply.github.com>
2019-11-03 19:31:16 +0100

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -22,7 +22,7 @@
         data: function() {
             return {
                 headline: 'Chi-Masters',
-                login: 'Einloggen oder Registrieren',
+                login: 'Login or register',
                 logoutText: 'Ausloggen',
                 loggedIn: App.data().loggedIn,
                 username: App.data().username
