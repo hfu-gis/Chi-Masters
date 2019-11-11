d3a5dd73223dd09865c711f772d7f381e42ea2b8
klotkawa <56807286+klotkawa@users.noreply.github.com>
2019-11-11 15:36:00 +0100

    Revert "Merge branch 'master' of https://github.com/hfu-gis/Chi-Masters"
    This reverts commit 834eec3e444b22e805cbfe3b357d10b8aaf8bd55, reversing
    changes made to 047a78c4dd3b85e0a289d532045553720ce31991.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="NavigationBar" role="navigation">
+        <div id="logo" role="img">
+            <router-link to="/views/home"><img alt="logo" src="../assets/logo.png"></router-link>
+        </div>
+        <div id="headline">
+            <h1>{{headline}}</h1>
+        </div>
+        <div id="navigation">
+            <router-link to="/views/login" v-if="!loggedIn">{{loginText}}</router-link>
+            <router-link to="/" v-if="loggedIn">{{logoutText}}</router-link>
+            <span v-if="loggedIn">{{username}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    import App from '../App.vue';
+
+    export default {
+        name: "Navbar",
+        data: function() {
+            return {
+                headline: 'Chi-Masters',
+                loginText: 'Login or register',
+                logoutText: 'Ausloggen',
+                loggedIn: App.data().loggedIn,
+                username: App.data().username
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #navigation {
+        margin-right: 2rem;
+    }
+    #NavigationBar {
+        height: 10vh;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        left: 0;
+        top: 0;
+        width: 100%;
+        box-shadow: 0 0.5vh 1vh grey;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="home">
+        <h1>Welcome</h1>
+        <md-button class="md-raised md-accent">GET STARTED</md-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+    #home {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+    }
+</style>
