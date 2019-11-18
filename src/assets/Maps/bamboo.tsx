858ea422673b721fe512e7db5d2e90977c77e442
SiewSchroeder <SiewSchroeder@users.noreply.github.com>
2019-11-18 18:48:21 +0100

    addin tilessheets

---
+++ src/assets/Maps/bamboo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="bamboo" tilewidth="32" tileheight="32" tilecount="221" columns="17">
+ <image source="C:/Users/kemsi/Downloads/bamboo.bmp" width="544" height="416"/>
+</tileset>
