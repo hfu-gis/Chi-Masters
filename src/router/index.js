4341505cd55068fb35956fca92d53b25c819da13
Dennis Hawran <dennishawran@gmail.com>
2019-11-25 15:16:33 +0100

    Uploaded new Project

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue';
+import Register from '../views/Register'
+import Home from '../views/Home.vue';
+import Calendar from "../views/Calendar";
+import Money from "../views/Money";
+import Settings from "../views/Settings";
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+
+    routes: [
+        {
+            path: '/views/Home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/views/Login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/views/Register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: '/views/Calendar',
+            name: 'calendar',
+            component: Calendar
+        },
+        {
+            path: '/views/Money',
+            name: 'money',
+            component: Money
+        },
+        {
+            path: '/views/Settings',
+            name: 'settings',
+            component: Settings
+        },
+        {
+            path: '*',
+            redirect: '/views/Home'
+        }
+
+    ]
+});
