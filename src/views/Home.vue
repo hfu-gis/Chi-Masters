094820282b3c2344a8c0fed824ef80a39c1cf2a0
SiewSchroeder <56884964+SiewSchroeder@users.noreply.github.com>
2019-12-09 14:49:56 +0100

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/hfu-gis/Chi-Masters""""
    This reverts commit 6014f677b33078e1fac68234df044c2019ed4042.

---
+++ src/views/Home.vue
@@ -0,0 +1,118 @@
+<template>
+
+    <div>
+        <v-card
+                class="mx-auto mb-6"
+                max-width="450"
+
+        >
+
+
+            <v-card-actions>
+                <v-container fluid>
+                    <v-list-item-title class="headline mb-3">Survey</v-list-item-title>
+                    <v-list-item-subtitle>Should we buy a new equipment? </v-list-item-subtitle>
+                    <v-row align="center">
+                        <v-col class="my-0" cols="12" sm="6">
+
+                            <v-select
+                                    :items="items"
+                                    label="Decide"
+                                    dense
+                                    solo
+                            ></v-select>
+                            <v-btn class="mr-2">
+                                <span class="mr-2">Send</span>
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+
+                </v-container>
+
+            </v-card-actions>
+        </v-card>
+
+        <v-card
+                class="mx-auto mb-6"
+                max-width="450"
+
+        >
+            <v-list-item three-line>
+                <v-list-item-content>
+                    <div class="overline mb-2">MONEY $</div>
+                    <v-list-item-title class="headline mb-3">Open amounts</v-list-item-title>
+                    <v-list-item-subtitle>Outstanding balance: 25$</v-list-item-subtitle>
+                </v-list-item-content>
+
+            </v-list-item>
+
+            <v-card-actions>
+                <router-link to="money" tag="v-btn">
+                    <v-btn class="ml-2 mb-2">
+                        <span class="mr-2">Money</span>
+                    </v-btn>
+                </router-link>
+            </v-card-actions>
+        </v-card>
+
+        <v-card
+                class="mx-auto mb-12"
+
+                max-width="450"
+        >
+            <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+                    :aspect-ratio="16/9"
+            >
+
+            </v-img>
+            <v-card-title>
+                <div class="display-1 mb-2">Next meeting</div>
+                <div class="title font-weight-regular grey--text">01.12.2019, Sunday, 14:30</div>
+            </v-card-title>
+
+            <v-divider class="mt-0"></v-divider>
+
+            <v-card-text>
+                <v-chip
+                        class="mr-2"
+                        @click="lights"
+                >
+                    <v-icon left>mdi-calendar-check</v-icon>
+                    Set in calendar
+                </v-chip>
+                <v-chip
+                        class="mr-2"
+                        @click="alarm"
+                >
+                    <v-icon
+                            color="green"
+                            left>mdi-check-circle</v-icon>
+                    Accept
+
+                </v-chip>
+                <v-chip @click="blinds">
+                    <v-icon
+                            color="red"
+                            left>mdi-close-circle</v-icon>
+                    Decline
+                </v-chip>
+            </v-card-text>
+        </v-card>
+    </div>
+
+
+</template>
+
+
+<script>
+    export default {
+        data: () => ({
+            items: ['Yes', 'No'],
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
